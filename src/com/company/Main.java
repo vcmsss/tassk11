8b18390fab152f7607f8c44b4662cd5e84984802
vcmsss <pak.al.ig@yandex.ru>
2019-07-25 21:18:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a string: ");
+
+        String s = in.nextLine();
+
+        System.out.println("Your string is " + s);
+    }
+}
